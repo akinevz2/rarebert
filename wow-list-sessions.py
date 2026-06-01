70edfb0a9400911409ef9a57f2863385a0ec5182
Kirill kine Nevzorov <akinevz@outlook.com>
2026-06-01 12:17:43 +0000

    utilities: add wow discussion modules

---
+++ wow-list-sessions.py
@@ -0,0 +1,14 @@
+"""wow-list-sessions: generated module scaffold."""
+
+from __future__ import annotations
+
+from devlib import run, todo
+
+
+def main() -> int:
+    todo("wow-list-sessions.main")
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(run(main))
